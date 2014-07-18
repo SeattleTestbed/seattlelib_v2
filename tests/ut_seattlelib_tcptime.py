d7c725398f47ee8e13362047c8fbfe3b0b325799
Albert <albert.rafetseder+github@univie.ac.at>
2014-07-18 21:34:17 +0200

    "Ported" tcptime unit test to Repy, added test for new error message in time_interface.
    * ut_seattlelib_tcptime.py used repyportability for no good reason.
    * ut_seattlelib_time_interface_updatetime_before_registermethod.r2py
      verifies that tcp_time complains when you try to update the time
      before registering a method at all.

--- tests/ut_seattlelib_tcptime.py
+++ tests/ut_seattlelib_tcptime.py
@@ -1,20 +1,14 @@
 """
-<Purpose>
-  Test the tcp_time library which now uses
-  Repy V2 network API calls.
-  Function Tested:
-    tcp_time_updatetime() in ntp_time.r2py
+Test the tcp_time library's tcp_time_updatetime().
 """
-from repyportability import *
-add_dy_support(locals())
-
+#pragma repy restrictions.default dylink.r2py
 time_interface = dy_import_module("time_interface.r2py")
 tcp_time = dy_import_module("tcp_time.r2py")
 
+timeport = list(getresources()[0]["connport"])[0]
+
 try:
-  time_interface.time_updatetime(12345)
-
+  time_interface.time_updatetime(timeport)
   time_interface.time_gettime()
-
 except time_interface.TimeError, err:
   log("[FAILED]: TCP time failed for following reason: " + str(err))
