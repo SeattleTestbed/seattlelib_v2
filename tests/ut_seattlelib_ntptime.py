b3ab0d909f7ac877544022546addabed4cfc19cf
Karthikeya Garimella <karthikgvss@users.noreply.github.com>
2015-01-28 00:21:05 -0500

    Update ut_seattlelib_ntptime.py

--- tests/ut_seattlelib_ntptime.py
+++ tests/ut_seattlelib_ntptime.py
@@ -3,11 +3,9 @@
   Test the ntp_time library which now uses
   Repy V2 network API calls.
   Function Tested:
-    ntp_time_updatetime() in ntp_time.r2py
+  ntp_time_updatetime() in ntp_time.r2py is indirectly tested / called from time_interface.time_updatetime() due to the dy_import_module("ntp_time.r2py") statement which registers the method as ntp
 """
 #pragma repy restrictions.default dylink.r2py    
-#from repyportability import *
-#add_dy_support(locals())
 
 time_interface = dy_import_module("time_interface.r2py")
 ntp_time = dy_import_module("ntp_time.r2py")
