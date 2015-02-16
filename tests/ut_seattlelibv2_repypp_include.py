5ae6d6954bb04e2562875e6f9a084b2c0cd7f023
Xuefeng <huangxuefengcn@gmail.com>
2015-02-15 23:42:37 -0500

    discard tempfile module and give more explanations
    Since the name using tempfile module which is not satisfied the requirement of Repy, so I discard it. And I give more explanations, modify file names. Also checking whether the file contents match what we expect.

--- tests/ut_seattlelibv2_repypp_include.py
+++ tests/ut_seattlelibv2_repypp_include.py
@@ -1,28 +1,43 @@
 """
-ut_seattlelibv2_repypp_include.py -- -- This script tests correct #include statements
+repypp.py is a preprocessor for repy. It includes dependent files as needed.This is used to help the 
+programmer avoid the need to use import. They can instead use "include X" which works somewhat like "from X import *".
 
+This script tests correct #include statements
 """
 
 import os
-import tempfile
 import subprocess
 
 def main():
     #create a temporary_file1 contains:
     #def foo():
-    # pass
-    temporary_file1 = tempfile.NamedTemporaryFile(prefix='testfile_', suffix='.py', dir= os.path.dirname(os.path.realpath(__file__)), delete=True)
-    temporary_file1.writelines(['def foo():\n','  pass'])
+    #  pass
+    temporary_file1 = open('testfile_repypp_example1.repy', 'w')
+    temporary_file1.write('def foo():\n  pass\n') 
+    temporary_file1.close()
 
     #create a temporary_file2 contains:
-    #include temporary_file1.py
+    #include testfile_repypp_example1.repy
     #def bar():
-    # pass
-    temporary_file2 = tempfile.NamedTemporaryFile(prefix='testfile_', suffix='.py', dir= os.path.dirname(os.path.realpath(__file__)), delete=True)
-    temporary_file2.writelines(['include ' + temporary_file1.name + '\n','def bar():\n','  pass'])
+    #  pass
+    temporary_file2 = open('testfile_repypp_example2.repy', 'w')
+    temporary_file2.write('include testfile_repypp_example1.repy\ndef bar():\n  pass\n') 
+    temporary_file2.close()
 
-    subprocess.call(['python', 'repypp.py',temporary_file2.name, 'unittest.py'])
-    os.remove('unittest.py')
+    subprocess.call(['python', 'repypp.py', 'testfile_repypp_example2.repy', 'testfile_repypp_example2_preprocessed.repy',])
+
+    # check whether the file contents match what we expect.
+    correctresult1 = 'def foo():\n  pass\n'
+    correctresult2 = 'def bar():\n  pass\n'
+    
+    f = open('testfile_repypp_example2_preprocessed.repy','r')
+
+    if not correctresult1 in f.read() and not correctresult2 in f.read():
+        print "the result of repypp.py produces is not correct!"
+
+    os.remove('testfile_repypp_example2_preprocessed.repy')
+    os.remove('testfile_repypp_example1.repy')
+    os.remove('testfile_repypp_example2.repy')
 
 if __name__ == "__main__":
   main()
