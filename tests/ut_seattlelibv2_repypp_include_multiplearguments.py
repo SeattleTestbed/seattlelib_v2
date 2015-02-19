04c89e931f741d01b0deab615eadc44ea609c8d3
Xuefeng <huangxuefengcn@gmail.com>
2015-02-19 11:08:51 -0500

    added test_repypp_library

--- tests/ut_seattlelibv2_repypp_include_multiplearguments.py
+++ tests/ut_seattlelibv2_repypp_include_multiplearguments.py
@@ -1,40 +1,47 @@
 #pragma out Error opening source file 'testfile_repypp_example1.repy testfile_repypp_example2.repy'
-"""
-repypp.py is a preprocessor for repy. It includes dependent files as needed.This is used to help the 
-programmer avoid the need to use import. They can instead use "include X" which works somewhat like "from X import *".
 
-This script tests if repypp.py check erroneous #include statements(have multiple arguments).
+"""
+repypp.py is a preprocessor for repy. It includes dependent files as 
+needed.This is used to help the programmer avoid the need to use 
+import. They can instead use "include X" which works somewhat like 
+"from X import *".
+
+This script tests if repypp.py check erroneous include statements
+(have multiple arguments).
 
 """
 
 import os
-import sys
-import subprocess
+import test_repypp_library
 
 def main():
+    filecontent1 = '''def foo1():
+  pass
+'''
+    filecontent2 = '''def foo2():
+  pass
+'''
+    filecontent3 = '''include testfile_repypp_example1.repy testfile_repypp_example2.repy
+def bar():
+  pass
+'''
     #create a temporary_file1 contains:
     #def foo1():
     #  pass 
-    temporary_file1 = open('testfile_repypp_example1.repy', 'w')
-    temporary_file1.write('def foo1():\n  pass\n') 
-    temporary_file1.close()
+    test_repypp_library.createtempfile('testfile_repypp_example1.repy', filecontent1)
 
     #create a temporary_file2 contains:
     #def foo2():
     #  pass
-    temporary_file2 = open('testfile_repypp_example2.repy', 'w')
-    temporary_file2.write('def foo2():\n  pass\n') 
-    temporary_file2.close()
+    test_repypp_library.createtempfile('testfile_repypp_example2.repy', filecontent2)
 
-    #create a temporary_file2 contains:
+    #create a temporary_file3 contains:
     #include testfile_repypp_example1.repy testfile_repypp_example2.repy
     #def bar():
     #  pass
-    temporary_file3 = open('testfile_repypp_example3.repy', 'w')
-    temporary_file3.write('include testfile_repypp_example1.repy testfile_repypp_example2.repy\ndef bar():\n  pass\n') 
-    temporary_file3.close()
+    test_repypp_library.createtempfile('testfile_repypp_example3.repy', filecontent3)
 
-    subprocess.call(['python', 'repypp.py', 'testfile_repypp_example3.repy', 'testfile_repypp_example3_preprocessed.repy']) 
+    test_repypp_library.preprocess('testfile_repypp_example3.repy')
 
     if os.path.isfile('testfile_repypp_example3_preprocessed.repy'):
      os.remove('testfile_repypp_example3_preprocessed.repy')
