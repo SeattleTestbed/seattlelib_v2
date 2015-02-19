e0b97ee2f072ccf4e5098c018c2bbec7125621dd
Xuefeng <huangxuefengcn@gmail.com>
2015-02-19 11:15:01 -0500

    added test_repypp_library

--- tests/ut_seattlelibv2_repypp_multiplearguments.py
+++ tests/ut_seattlelibv2_repypp_multiplearguments.py
@@ -5,36 +5,46 @@
 #pragma out written to outfile.   Outfile and infile must be distinct.
 
 """
-repypp.py is a preprocessor for repy. It includes dependent files as needed.This is used to help the 
-programmer avoid the need to use import. They can instead use "include X" which works somewhat like "from X import *".
+repypp.py is a preprocessor for repy. It includes dependent files as 
+needed.This is used to help the programmer avoid the need to use 
+import. They can instead use "include X" which works somewhat like 
+"from X import *".
 
-This script tests if repypp.py check erroneous arguments(have multiple arguments).
+This script tests if repypp.py check command line erroneous arguments
+(have multiple arguments).
 
 """
 
 import os
-import sys
-import tempfile
 import subprocess
+import sys
+import test_repypp_library
+
 
 def main():
+    filecontent1 = '''def foo():
+  pass
+'''
+    filecontent2 = '''include testfile_repypp_example1.repy
+def bar():
+  pass
+'''
     #create a temporary_file1 contains:
     #def foo():
     #  pass
-    temporary_file1 = open('testfile_repypp_example1.repy', 'w')
-    temporary_file1.write('def foo():\n  pass\n') 
-    temporary_file1.close()
+    test_repypp_library.createtempfile('testfile_repypp_example1.repy', filecontent1)
 
     #create a temporary_file2 contains:
     #include testfile_repypp_example1.repy
     #def bar():
     #  pass
-    temporary_file2 = open('testfile_repypp_example2.repy', 'w')
-    temporary_file2.write('include testfile_repypp_example1.repy\ndef bar():\n  pass\n') 
-    temporary_file2.close()
-
-    subprocess.call(['python', 'repypp.py', 'testfile_repypp_example2.repy', 'testfile_repypp_example2_preprocessed.repy' , 'testfile_repypp_example2.repy'])   
+    test_repypp_library.createtempfile('testfile_repypp_example2.repy', filecontent2)
     
+    try:
+        subprocess.call([sys.executable, 'repypp.py', 'testfile_repypp_example2.repy', 'testfile_repypp_example2_preprocessed.repy' , 'testfile_repypp_example2.repy'])   
+    except:
+        print 'Can not execute repypp.py'
+
     if os.path.isfile('testfile_repypp_example2_preprocessed.repy'):
         os.remove('testfile_repypp_example2_preprocessed.repy')
         
