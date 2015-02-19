6fc3f2b248559a1266caa22d57591f08a4ad788a
Xuefeng <huangxuefengcn@gmail.com>
2015-02-19 11:11:05 -0500

    added test_repypp_library

--- tests/ut_seattlelibv2_repypp_include_noargument.py
+++ tests/ut_seattlelibv2_repypp_include_noargument.py
@@ -1,25 +1,32 @@
 #pragma out Error opening source file ''
-"""
-repypp.py is a preprocessor for repy. It includes dependent files as needed.This is used to help the 
-programmer avoid the need to use import. They can instead use "include X" which works somewhat like "from X import *".
 
-This script tests if repypp.py check erroneous #include statements(have no arguments at all).
+"""
+repypp.py is a preprocessor for repy. It includes dependent files as 
+needed.This is used to help the programmer avoid the need to use 
+import. They can instead use "include X" which works somewhat like 
+"from X import *".
+
+This script tests if repypp.py check erroneous include statements
+(have no arguments at all).
 """
 
 import os
-import sys
-import subprocess
+import test_repypp_library
 
 def main():
-    #create a temporary_file2 contains:
+    filecontent = '''include 
+def bar():
+  pass
+'''
+    if os.path.isfile('testfile_repypp_example.repy'):
+        os.remove('testfile_repypp_example.repy')
+    #create a temporary_file contains:
     #include 
     #def bar():
     #  pass
-    temporary_file = open('testfile_repypp_example.repy', 'w')
-    temporary_file.write('include  \ndef bar():\n  pass\n') 
-    temporary_file.close()
+    test_repypp_library.createtempfile('testfile_repypp_example.repy', filecontent)
 
-    subprocess.call(['python', 'repypp.py', 'testfile_repypp_example.repy', 'testfile_repypp_example_preprocessed.repy']) 
+    test_repypp_library.preprocess('testfile_repypp_example.repy')
     
     if os.path.isfile('testfile_repypp_example_preprocessed.repy'):
         os.remove('testfile_repypp_example_preprocessed.repy')
